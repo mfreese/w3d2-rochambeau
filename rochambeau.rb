77de7acbf91703a213c5715c7000edffbdbb76fa
Michael Freese <mrfreese1@gmail.com>
2016-10-11 14:17:08 -0400

    second

--- rochambeau.rb
+++ rochambeau.rb
@@ -1,14 +1,14 @@
 class Game
   attr_accessor :player_one_choice, :computer
 
-def initialize (player_one_choice, computer)
+def initialize
   self.computer = computer
 end
 def play
 
 puts "Lets play Rochambeau"
 puts "Player choose rock, paper, scissors"
-gets.chomp
+
 
 player_one_choice=gets.chomp
 computer = %w(rock paper scissors).sample
@@ -20,9 +20,28 @@ elsif
 elsif
   (player_one_choice == "rock") && (computer == "rock")
   puts "Player: ROCK vs Computer: ROCK ----- IT'S A DRAW"
+elsif
+  (player_one_choice == "paper") && (computer == "rock")
+  puts "Player: PAPER vs Computer: ROCK ----- PLAYER WINS"
+elsif
+  (player_one_choice == "paper") && (computer == "paper")
+  puts "Player: PAPER vs Computer: PAPER ----- ITS A DRAW"
+elsif
+  (player_one_choice == "paper") && (computer == "scissors")
+  puts "Player: PAPER vs Computer: SCISSORS ----- COMPUTER WINS"
+elsif
+  (player_one_choice == "scissors") && (computer == "scissors")
+  puts "Player: SCISSORS vs Computer: SCISSORS ----- ITS A DRAW"
+elsif
+  (player_one_choice == "scissors") && (computer == "rock")
+  puts "Player: SCISSORS vs Computer: ROCK ----- COMPUTER WINS"
+elsif
+  (player_one_choice == "scissors") && (computer == "paper")
+  puts "Player: SCISSORS vs Computer: PAPER ----- PLAYER WINS"
+else
+  puts "Please choose rock, paper, or scissors."
 end
-
 end
-
+end
 game = Game.new
 game.play # %w(rock paper scissors) == ["rock", "paper", "scissors"]
